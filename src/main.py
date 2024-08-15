4176b627fb1a9b53081346672383aa0f54d3aa8e
TaitA2 <adam.tait617@gmail.com>
2024-08-15 16:25:50 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-def get_variables():
-    # variables
+def main():
+    # fees dict
+    fees = {} 
 
     # default fee value
-    fee = 200
+    base_fee = 200
     # travel_tier 0-3
     travel_tier = int(input("Enter travel tier (0-3)..."))
     
@@ -16,21 +17,22 @@ def get_variables():
     # default fuel price per litre (€)
     fuel_rate = 1.82
     # distance in km
-    distance = int(input("Enter distance (km) one way...")) * 2
-    distance = distance
+    carpooling = input("Carpooling? (y/n)")
+    if carpooling.upper() == "Y":
+        distance = int(input("Enter distance (km) one way...")) * 2
+    elif carpooling.upper() == "N":
+        distance = int(input("Enter distance (km) one way..."))
+    else:
+        raise Exception("Invalid answer")      
     # total litres
     litres = distance / fuel_efficiency
     # total cost 
     fuel_total = round(litres * fuel_rate, 2)
 
     # total of all expenses
-    grand_total = fee + fuel_total + tolls
-    return fee, travel_tier, fuel_total, grand_total     # TODO should probably make it return dict eg. {"fee": fee}
-    
+    grand_total = base_fee + fuel_total + tolls
 
-def main():
-    fee, travel_tier, fuel_total, grand_total = get_variables()
-    print(f"BASE FEE: €{fee}")
+    print(f"BASE FEE: €{base_fee}")
     print(f"TRAVEL TIER {travel_tier}: €{travel_tier * 20}")
     print(f"FUEL: €{fuel_total}")
     print(f"TOTAL: €{grand_total}")
