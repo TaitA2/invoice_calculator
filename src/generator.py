3a7955ce3191b07738889e45a09438ba18089d50
TaitA2 <adam.tait617@gmail.com>
2024-08-17 19:24:13 +0100

    modified template to use place_holders to be replaced during invoice generation

--- src/generator.py
+++ src/generator.py
@@ -2,7 +2,7 @@ import docx
 # function to create a new invoice from a docx template
 def generate_invoice():
     template = docx.Document("template.docx")
-    template.save("test.docx")
+    template.save("invoices/test.docx")
 
 if __name__ == "__main__":
     generate_invoice()
