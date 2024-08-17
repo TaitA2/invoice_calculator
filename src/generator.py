8f5a1244822a4c02a64ec9d8c72ffe111eb85a30
TaitA2 <adam.tait617@gmail.com>
2024-08-17 19:11:58 +0100

    testing docx file saving

--- src/generator.py
+++ src/generator.py
@@ -1,11 +1,8 @@
-import PyPDF2 as pdf2
-# function to create a new invoice from a template
+import docx
+# function to create a new invoice from a docx template
 def generate_invoice():
-    # create pdf reader object
-    reader = pdf2.PdfReader("template.pdf")
-    template = reader.pages[0]
-    text = template.extract_text()
-    pdf2.addText()
-    # print the text in the template as confirmation
-    print(text) 
+    template = docx.Document("template.docx")
+    template.save("test.docx")
 
+if __name__ == "__main__":
+    generate_invoice()
